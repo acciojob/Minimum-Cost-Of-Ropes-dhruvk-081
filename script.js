ecd8ffda3aba50224cca68009eed0dcf6aa128b1
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-02-23 12:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-function minimumCost() {
-    
-    let n = arr.length;
-  if (n < 2) return 0;
-  
-  // Create a priority queue
-  let pq = new PriorityQueue();
-  for (let i = 0; i < n; i++) {
-    pq.enqueue(arr[i]);
-  }
-  
-  // Start connecting ropes
-  let totalCost = 0;
-  while (pq.size() > 1) {
-    let rope1 = pq.dequeue();
-    let rope2 = pq.dequeue();
-    let newRope = rope1 + rope2;
-    totalCost += newRope;
-    pq.enqueue(newRope);
-  }
-  
-  return totalCost;
-}
+const val = document.querySelector('#rope-lengths');
+const result = document.getElementById("result")
 
-class PriorityQueue {
-  constructor() {
-    this.items = [];
-  }
-  
-  enqueue(item) {
-    let i = 0;
-    while (i < this.items.length && item > this.items[i]) {
-      i++;
-    }
-    this.items.splice(i, 0, item);
-  }
-  
-  dequeue() {
-    return this.items.shift();
-  }
-  
-  size() {
-    return this.items.length;
-  }
-} 
+function calculateMinCost() {
+	const arr = (val.value).split(", ").map((e) => parseInt(e));
+	const n = arr.length
+	 let pq = [];
+   
+        // Adding items to the pQueue
+        for (let i = 0; i < n; i++) {
+            pq.push(arr[i]);
+        }   
+           
+        pq.sort(function(a,b){return a-b;});
+         
+        // Initialize result
+        let res = 0;
+   
+        // While size of priority queue
+        // is more than 1
+        while (pq.length > 1) {
+            // Extract shortest two ropes from pq
+            let first = pq.shift();
+            let second = pq.shift();
+   
+            // Connect the ropes: update result
+            // and insert the new rope to pq
+            res += first + second;
+            pq.push(first + second);
+            pq.sort(function(a,b){return a-b;});
+        }
+
+	
+	   result.innerText = res;
+        return res;
+}
