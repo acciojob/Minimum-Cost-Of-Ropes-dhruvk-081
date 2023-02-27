fc17a2ecdd40d43ddf64e7792714368b6e636cd2
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-02-27 13:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-const val = document.querySelector('#rope-lengths');
-const result = document.getElementById("result")
-
 function calculateMinCost() {
-	const arr = (val.value).split(", ").map((e) => parseInt(e));
-	const n = arr.length
-	 let pq = [];
-   
-        // Adding items to the pQueue
-        for (let i = 0; i < n; i++) {
-            pq.push(arr[i]);
-        }   
-           
-        pq.sort(function(a,b){return a-b;});
-         
-        // Initialize result
-        let res = 0;
-   
-        // While size of priority queue
-        // is more than 1
-        while (pq.length > 1) {
-            // Extract shortest two ropes from pq
-            let first = pq.shift();
-            let second = pq.shift();
-   
-            // Connect the ropes: update result
-            // and insert the new rope to pq
-            res += first + second;
-            pq.push(first + second);
-            pq.sort(function(a,b){return a-b;});
-        }
+  const value = document.getElementById('rope-lengths').value;
+  const arr = value.split(',').map(Number);
+  const n = arr.length;
 
-	
-	   result.innerText = res;
-        return res;
+  let pq = [];
+  for (let i = 0; i < n; i++) {
+    pq.push(arr[i]);
+  }
+  pq.sort(function (a, b) { return a - b; });
+
+  let res = 0;
+  while (pq.length > 1) {
+    let first = pq.shift();
+    let second = pq.shift();
+    res += first + second;
+    pq.push(first + second);
+    pq.sort(function (a, b) { return a - b; });
+  }
+
+  document.getElementById('result').innerHTML = res;
 }
